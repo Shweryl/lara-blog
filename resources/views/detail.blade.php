1cc3cbcb9e321486c3566ea6da043074719d4625
veryl <shwezin2001shwezin2001@gmail.com>
2022-08-31 09:37:31 +0630

    ready to git

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -3,27 +3,49 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12 col-lg-8">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <h3 class="mb-0">{{$post->title}}</h3>
-                        <div class="">
-                            <a href="#">
-                                    <span class="badge bg-secondary">
+                <div class="mb-3">
+                    <h3 class=" text-center">{{$post->title}}</h3>
+                    <div class="text-center">
+                        <a href="#">
+                                    <span class="badge bg-secondary ">
                                         {{$post->category->title}}
                                     </span>
-                            </a>
-                        </div>
-                        <div class="my-3">
-                            @foreach($post->photos as $photo)
-                                <img src="{{asset('storage/'.$photo->name)}}" height="100" class="rounded" alt="">
-                            @endforeach
-                        </div>
-                        <p class="my-3">{{$post->description}}</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="">
-                                <p class="mb-0 text-black-50 fw-bold ">{{$post->user->name}}</p>
-                                <p class="mb-0 text-secondary">{{$post->created_at->diffforhumans()}}</p>
+                        </a>
+                    </div>
+                    <div class="my-3">
+                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                @foreach($post->photos as $key=>$photo)
+                                    <div class="carousel-item {{$key===0?'active':''}}">
+                                        <a class="venobox" data-gall="myGallery" href="{{asset('storage/'.$photo->name)}}">
+                                            <img src="{{asset('storage/'.$photo->name)}}" class="d-block post-detail-img w-100">
+                                        </a>
+
+                                    </div>
+                                @endforeach
                             </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon text-primary" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                    <p class="my-3" style="white-space:pre-wrap">{{$post->description}}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="">
+                            <p class="mb-0 text-black-50 fw-bold ">{{$post->user->name}}</p>
+                            <p class="mb-0 text-secondary">{{$post->created_at->diffforhumans()}}</p>
+                        </div>
+                        <div class="">
+                            @can('update',$post)
+                                <a href="{{route('post.edit',$post->id)}}" class="btn btn btn-outline-primary">
+                                    <i class="bi bi-pencil"></i>
+                                </a>
+                            @endcan
                             <a href="{{route('page.index')}}" class="btn btn-primary">
                                 All Posts
                             </a>
